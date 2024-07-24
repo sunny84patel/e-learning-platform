5dd6902f9b9ecfae8fb852e14b56e3fc713020bd
sunny84patel <88608402+sunny84patel@users.noreply.github.com>
2024-07-24 11:55:07 +0530

    first commit

---
+++ back-end/omega-backend/models/EnrolledUsers.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const enrolledUserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone:{type: String, required: true, unique: true }
+});
+
+module.exports = mongoose.model('EnrolledUser', enrolledUserSchema);
